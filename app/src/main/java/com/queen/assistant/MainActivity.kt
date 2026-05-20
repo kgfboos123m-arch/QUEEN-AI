5ca33b442f83637db8cef6eb45fa771eb4ca7b79
kgfboos123m-arch <kgfboos123m@gmail.com>
2026-05-20 13:00:54 +0530

    Refactor MainActivity to use Activity and update UI
    Changed MainActivity to extend Activity instead of AppCompatActivity and updated UI to display a centered text message.

--- app/src/main/java/com/queen/assistant/MainActivity.kt
+++ app/src/main/java/com/queen/assistant/MainActivity.kt
@@ -1,9 +1,22 @@
 package com.queen.assistant
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+import android.view.Gravity
+import android.graphics.Color
+
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        val textView = TextView(this)
+        textView.text = "QUEEN IS READY!"
+        textView.textSize = 30f
+        textView.setTextColor(Color.RED)
+        textView.setBackgroundColor(Color.BLACK)
+        textView.gravity = Gravity.CENTER
+        
+        setContentView(textView)
     }
 }
