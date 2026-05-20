8bdc781ec909eccba74157862a5318e11fb48f38
kgfboos123m-arch <kgfboos123m@gmail.com>
2026-05-20 12:22:02 +0530

    Create MainActivity class for the application

---
+++ MainActivity.kt
@@ -0,0 +1,9 @@
+package com.queen.assistant
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
